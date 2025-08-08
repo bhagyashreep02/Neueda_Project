44a11703c2efbd89bba3a4900d537cc95457e5d8
shreyamote <shreyamote.1573@gmail.com>
2025-08-08 12:03:08 +0530

    added OLS Multiple Regression

--- rest-api/src/main/java/com/sj/springboot/rest_api/service/MLService.java
+++ rest-api/src/main/java/com/sj/springboot/rest_api/service/MLService.java
@@ -1,23 +1,42 @@
 package com.sj.springboot.rest_api.service;
 
-import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MLService {
 
     public double predictNextPrice(List<Double> prices) {
-        if (prices == null || prices.size() < 2) {
-            return Double.NaN; // Not enough data
+        if (prices == null || prices.size() < 5) {
+            return Double.NaN; // not enough data
         }
 
-        SimpleRegression regression = new SimpleRegression();
-        for (int i = 0; i < prices.size(); i++) {
-            regression.addData(i, prices.get(i));
+        List<Double> diffSeries = new ArrayList<>();
+        for (int i = 1; i < prices.size(); i++) {
+            diffSeries.add(prices.get(i) - prices.get(i - 1));
         }
 
-        return regression.predict(prices.size()); // next day's index
+        int n = diffSeries.size() - 1;
+        double[] y = new double[n];
+        double[][] x = new double[n][1];
+
+        for (int i = 0; i < n; i++) {
+            y[i] = diffSeries.get(i + 1);
+            x[i][0] = diffSeries.get(i);
+        }
+
+        OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+        regression.newSampleData(y, x);
+
+        double[] beta = regression.estimateRegressionParameters();
+
+        double lastDiff = diffSeries.get(diffSeries.size() - 1);
+        double forecastDiff = beta[0] + beta[1] * lastDiff;
+
+        double lastActualPrice = prices.get(prices.size() - 1);
+        return lastActualPrice + forecastDiff;
     }
 }
